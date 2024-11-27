386a8f1b265c8e6c57c891dc3424bc140590720e
BELYAHIAOUI Hamza ADMIN <hamza.belyahiaoui@FRLT284411.ce.int.amecspie.com>
2024-11-27 11:39:26 +0100

    fix

--- Api/Program.cs
+++ Api/Program.cs
@@ -36,11 +36,11 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 
 //app.UseHttpsRedirection();
